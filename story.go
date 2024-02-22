9093ee63c855b886347511d7658e2fc1c7db711d
Matt Fenwick <952655+mattfenwick@users.noreply.github.com>
2024-02-22 14:36:50 -0500

    Create story.go

---
+++ story.go
@@ -0,0 +1,7 @@
+who were in love
+
+Title: Two Germans in Love
+
+It was the year 1940, and the world was at war. Germany had invaded Poland, and the country was in chaos. Amidst the turmoil, two young Germans, Hans and Anna, found each other in the midst of it all. They were from different backgrounds, Hans from a working-class family and Anna from a wealthy one, but they had one thing in common - their love for each other.
+
+Hans was a soldier in the German army, stationed in Poland. He had been sent to fight in the war, but he never wanted to leave Anna's side. He would write her letters every day, telling her about his experiences and how much he missed her. Anna, on the other hand, was a nurse in a hospital in Berlin. She had volunteered to help the wounded soldiers, and she was determined to do
