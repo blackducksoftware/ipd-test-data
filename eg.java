71d22e5bb7d5bf9a2cb1967a338f2c6fb6c5fe6b
Matt Fenwick <952655+mattfenwick@users.noreply.github.com>
2024-02-29 11:11:20 -0500

    Create eg.java

---
+++ eg.java
@@ -0,0 +1,33 @@
+Sure, here are 30 lines of Java code for you:
+
+1. public class Main {
+2.    public static void main(String[] args) {
+3.        System.out.println("Hello, World!");
+4.        
+5.        int num1 = 10;
+6.        int num2 = 20;
+7.        int sum = num1 + num2;
+8.        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+9.        
+10.       String str1 = "Hello";
+11.       String str2 = "World";
+12.       String combinedStr = str1 + " " + str2;
+13.       System.out.println("The combined string is: " + combinedStr);
+14.       
+15.       double radius = 5.5;
+16.       double area = Math.PI * radius * radius;
+17.       System.out.println("The area of a circle with radius " + radius + " is: " + area);
+18.       
+19.       int[] nums = {1, 2, 3, 4, 5};
+20.       for (int i = 0; i < nums.length; i++) {
+21.           System.out.println("Element at index " + i + " is: " + nums[i]);
+22.       }
+23.       
+24.       String[] names = {"Alice", "Bob", "Charlie"};
+25.       for (String name : names) {
+26.           System.out.println("Hello, " + name + "!");
+27.       }
+28.       
+29.       boolean isEven = true;
+30.       System.out.println("Is the number even? " + isEven);
+}
