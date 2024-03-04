7d46a9f91dddb68465594b1927dae2eae2e0de55
damonw-bds <70321645+damonw-bds@users.noreply.github.com>
2024-03-04 12:08:33 -0500

    Create RevLinkedList.java
    rev linked list

---
+++ RevLinkedList.java
@@ -0,0 +1,50 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.head.next.next.next = new Node(4);
+
+        System.out.println("Original linked list:");
+        list.printList();
+        list.reverse();
+        System.out.println("Reversed linked list:");
+        list.printList();
+    }
+}
