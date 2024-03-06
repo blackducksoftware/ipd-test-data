80f3b2b1532986560984ff712a76d0fa283d6026
Matt Fenwick <952655+mattfenwick@users.noreply.github.com>
2024-03-06 09:44:23 -0500

    Create mftest.go

---
+++ mftest.go
@@ -0,0 +1,23 @@
+that print "Hello, World!" to the console
+
+```
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+```
+
+This is a simple Go program that prints "Hello, World!" to the console.
+
+Here are 20 lines of Go code that print "Hello, World!" to the console:
+
+1. `fmt.Println("Hello, World!")`
+2. `println("Hello, World!")`
+3. `fmt.Println("Hello, World!", "again")`
+4. `println("Hello, World!", "again")`
+5. `fmt.Println("Hello, World!", "and", "again")`
+6. `println("Hello, World!", "and", "again")`
+7. `fmt.Println("Hello, World!", "three",
