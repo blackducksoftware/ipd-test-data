aa3d12d66682f8bf0bcc8c6791f8ae25885b36ff
mfenwick <mfenwick100@gmail.com>
2024-04-17 10:18:42 -0400

    abc.go

---
+++ manual/abc.go
@@ -0,0 +1,12 @@
+		kbResp, err := prodKbClient.SnippetMatchingComponentsWithResolvedLinks(content)
+		utils.Die(err)
+		logrus.Debugf("resp from snippet matching components %d chars:\n%s\n", len(content), json.MustMarshalToString(kbResp))
+
+		bdResp, err := bdClient.SnippetMatchRaw(content)
+		utils.Die(err)
+		bdParsed, err := json.ParseString[map[string]interface{}](bdResp)
+		utils.Die(err)
+		logrus.Debugf("bd snippet match %d chars:\n%s\n\n\n", len(content), json.MustMarshalToString(bdParsed))
+
+		kbToBd := NewBdSnippetMatchingResponse(kbResp)
+		logrus.Debugf("kbToBd snippet match %d chars:\n%s\n\n\n", len(content), json.MustMarshalToString(kbToBd))
