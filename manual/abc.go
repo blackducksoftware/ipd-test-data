6a877d8388052b9bc5495673e7b1f5d1e1fa8ce9
mfenwick <mfenwick100@gmail.com>
2024-04-17 10:30:42 -0400

    some copy/paste

--- manual/abc.go
+++ manual/abc.go
@@ -10,3 +10,19 @@
 
 		kbToBd := NewBdSnippetMatchingResponse(kbResp)
 		logrus.Debugf("kbToBd snippet match %d chars:\n%s\n\n\n", len(content), json.MustMarshalToString(kbToBd))
+
+
+                kbResp, err := prodKbClient.SnippetMatchingComponentsWithResolvedLinks(content)
+                utils.Die(err)
+                logrus.Debugf("resp from snippet matching components %d chars:\n%s\n", len(content), json.MustMarshalToString(kbR\
+esp))
+
+                bdResp, err := bdClient.SnippetMatchRaw(content)
+                utils.Die(err)
+                bdParsed, err := json.ParseString[map[string]interface{}](bdResp)
+                utils.Die(err)
+                logrus.Debugf("bd snippet match %d chars:\n%s\n\n\n", len(content), json.MustMarshalToString(bdParsed))
+
+                kbToBd := NewBdSnippetMatchingResponse(kbResp)
+                logrus.Debugf("kbToBd snippet match %d chars:\n%s\n\n\n", len(content), json.MustMarshalToString(kbToBd))
+
